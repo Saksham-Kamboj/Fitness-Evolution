a50c2df53e082babfb706d87ad0ebe5f9a0c8325
SAKSHAM KAMBOJ <111846740+Saksham-Kamboj@users.noreply.github.com>
2024-07-01 19:25:55 +0530

    update 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"; //Gruppo
+import { Inter as FontSans } from "next/font/google";
 import "@/resource/styles/globals.css";
 import { cn } from "@/lib/utils";
 import NextAuthProvider from "@/core/providers/Provider";
